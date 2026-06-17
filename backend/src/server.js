be204d6df470730b03e3b7400d031b85a7a9db92
GARVITJAIN-1 <23ucs578@lnmiit.ac.in>
2026-06-18 00:04:38 +0530

    server authentication protection

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,14 +4,19 @@ import { connectDB } from "./lib/db.js"
 import {serve} from "inngest/express"
 import cors from "cors"
 import { inngest,functions } from "./lib/inngest.js"
+import {clerkMiddleware} from '@clerk/express'
 import path from "path"
+import { protectRoute } from "./middleware/protectRoute.js"
+import chatRoutes from "./routes/chatRoutes.js"
 
 const app=express()
 const __dirname=path.resolve()
 //middleware
 app.use(express.json())
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+app.use(clerkMiddleware())
 app.use("/api/inngest",serve({client:inngest,functions}))
+app.use("/api/chat",chatRoutes)
 
 app.get("/api/health",(req,res)=>{
     res.status(200).json({
